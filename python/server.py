97ba033d69599203baa639eb312f5e23f0d5e8bc
Joy <521171942@qq.com>
2021-03-14 10:26:12 +0800

    其他

--- python/server.py
+++ python/server.py
@@ -10,4 +10,5 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
         print('Connected by', addr)
         while True:
             data = conn.recv(1024)
+            print('receive:', data)
             conn.sendall(data)
