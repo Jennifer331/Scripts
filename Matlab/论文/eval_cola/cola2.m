00d12612185a9e45878e2e5765d6501817716c7b
Joy <521171942@qq.com>
2021-04-13 00:37:04 +0800

    论文画图

--- Matlab/论文/eval_cola/cola2.m
+++ Matlab/论文/eval_cola/cola2.m
@@ -9,27 +9,27 @@ markers = ["o-", "*", ".-"];
 figure(1)
 hold on
 for i=1:2
-    eval(sprintf("plot(freqs, unwrap(d%d_cola_phase_f), markers(%d), 'color', 'r')", dists(i), i))
-    eval(sprintf("plot(freqs, unwrap(d%d_cola_phase_t), markers(%d), 'color', 'r', 'MarkerSize', 3)", dists(i), i))
-    eval(sprintf("plot(freqs, unwrap(d%d_colanosugar_phase_f), markers(%d), 'color', 'k')", dists(i), i))
-    eval(sprintf("plot(freqs, unwrap(d%d_colanosugar_phase_t), markers(%d), 'color', 'k', 'MarkerSize', 3)", dists(i), i))
+    eval(sprintf("plot(freqs, unwrap(d%d_cola_phase_f), markers(%d), 'color', colors(1,:))", dists(i), i))
+    eval(sprintf("plot(freqs, unwrap(d%d_cola_phase_t), markers(%d), 'color', colors(1,:), 'MarkerSize', 3)", dists(i), i))
+    eval(sprintf("plot(freqs, unwrap(d%d_colanosugar_phase_f), markers(%d), 'color', colors(2,:))", dists(i), i))
+    eval(sprintf("plot(freqs, unwrap(d%d_colanosugar_phase_t), markers(%d), 'color', colors(2,:), 'MarkerSize', 3)", dists(i), i))
 end
 hold off
 legend('有糖-位置1-正', '有糖-位置1-反', '无糖-位置1-正', '无糖-位置1-反', '有糖-位置2-正', '有糖-位置2-反', '无糖-位置2-正', '无糖-位置2-反')
 xlabel('频道/（MHz）')
 ylabel('相位/（rad）')
-% 
-% set(gcf, 'PaperPosition', [0 0 16 12])
-% set(gcf, 'PaperSize', [16 12])
-% saveas(gcf, 'cola_phase1.pdf')
+
+set(gcf, 'PaperPosition', [0 0 16 12])
+set(gcf, 'PaperSize', [16 12])
+saveas(gcf, 'cola_phase2.pdf')
 %% RSSI
 figure(2)
 for i=1:2
-    eval(sprintf("plot(freqs, d%d_cola_rssi_f, markers(%d), 'color', 'r')", dists(i), i))
+    eval(sprintf("plot(freqs, d%d_cola_rssi_f, markers(%d), 'color', colors(1,:))", dists(i), i))
     hold on
-    eval(sprintf("plot(freqs, d%d_cola_rssi_t, markers(%d), 'color', 'r', 'MarkerSize', 3)", dists(i), i))
-    eval(sprintf("plot(freqs, d%d_colanosugar_rssi_f, markers(%d), 'color', 'k')", dists(i), i))
-    eval(sprintf("plot(freqs, d%d_colanosugar_rssi_t, markers(%d), 'color', 'k', 'MarkerSize', 3)", dists(i), i))
+    eval(sprintf("plot(freqs, d%d_cola_rssi_t, markers(%d), 'color', colors(1,:), 'MarkerSize', 3)", dists(i), i))
+    eval(sprintf("plot(freqs, d%d_colanosugar_rssi_f, markers(%d), 'color', colors(2,:))", dists(i), i))
+    eval(sprintf("plot(freqs, d%d_colanosugar_rssi_t, markers(%d), 'color', colors(2,:), 'MarkerSize', 3)", dists(i), i))
 end
 hold off
 legend('有糖-位置1-正', '有糖-位置1-反', '无糖-位置1-正', '无糖-位置1-反', '有糖-位置2-正', '有糖-位置2-反', '无糖-位置2-正', '无糖-位置2-反')
@@ -38,23 +38,23 @@ ylabel('接收信号强度/（dBm）')
 
 % set(gcf, 'PaperPosition', [0 0 16 12])
 % set(gcf, 'PaperSize', [16 12])
-% saveas(gcf, 'cola_rssi1.pdf')
+% saveas(gcf, 'cola_rssi2.pdf')
 %% diff
 figure(3)
 
 markers = ["o", "*", "^"];
 hold on
 for i=1:3
-    eval(sprintf("plot(unwrap(d%d_cola_phase_f) - unwrap(d%d_cola_phase_t), d%d_cola_rssi_f - d%d_cola_rssi_t, markers(%d), 'color', 'r')", dists(i),dists(i), dists(i), dists(i), i))
-    eval(sprintf("plot(unwrap(d%d_colanosugar_phase_f) - unwrap(d%d_colanosugar_phase_t), d%d_colanosugar_rssi_f - d%d_colanosugar_rssi_t, markers(%d), 'color', 'k')", dists(i),dists(i), dists(i), dists(i), i))
+    eval(sprintf("plot(unwrap(d%d_cola_phase_f) - unwrap(d%d_cola_phase_t), d%d_cola_rssi_f - d%d_cola_rssi_t, markers(%d), 'color', colors(1,:))", dists(i),dists(i), dists(i), dists(i), i))
+    eval(sprintf("plot(unwrap(d%d_colanosugar_phase_f) - unwrap(d%d_colanosugar_phase_t), d%d_colanosugar_rssi_f - d%d_colanosugar_rssi_t, markers(%d), 'color', colors(2,:))", dists(i),dists(i), dists(i), dists(i), i))
 end
 hold off
-legend('水', '醋', '牛奶', '酸奶', '油', '白酒')
+legend('位置1-有糖', '位置1-无糖', '位置2-有糖', '位置2-无糖', '位置3-有糖', '位置3-无糖')
 xlabel('相位差/（rad）')
 ylabel('接收信号强度差/（dB）')
 xlim([-6.28, 6.28])
 ylim([-6, 3])
 
-% set(gcf, 'PaperPosition', [0 0 16 12])
-% set(gcf, 'PaperSize', [16 12])
-% saveas(gcf, 'cola_feature1.pdf')
+set(gcf, 'PaperPosition', [0 0 16 12])
+set(gcf, 'PaperSize', [16 12])
+saveas(gcf, 'cola_feature2.pdf')
